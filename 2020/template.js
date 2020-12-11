f7c6a35d38fc839dd110c771c63f58a09df755a8
Svenn-Ivar Svendsen <svennivar.svendsen@microchip.com>
2020-12-11 13:21:23 +0100

    Added template

---
+++ 2020/template.js
@@ -0,0 +1,16 @@
+const solvePuzzle = (title, input) => {
+    console.log("-", title, "-");
+
+    
+
+    // console.log("1:", );
+    // console.log("2:", );
+}
+
+solvePuzzle("Day * example", 
+``
+);
+
+//solvePuzzle("Day * challenge",
+``
+//);

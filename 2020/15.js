ca4be6bc267875be710c019f2b0d9f2f16a6a594
Svenn-Ivar Svendsen <svennivar.svendsen@microchip.com>
2020-12-15 13:13:48 +0100

    Simple measurements

--- 2020/15.js
+++ 2020/15.js
@@ -1,33 +1,48 @@
+
+const { performance } = require('perf_hooks');
+
 const solve = (input) => {
 
-    const numbers = input.split(",").map(n => +n);
+    const startingNumbers = input.split(",").map(n => +n);
 
     const run = (turns) => {
 
-        // Track the last two turns each number has been spoken.  
-        // Two or one 0 values means the number was not spoken yet or only once
-        const lastSpokenTurnPairs = new Uint32Array(turns * 2);
-        numbers.forEach((number, index) => lastSpokenTurnPairs[number * 2] = index + 1);
+        const start = performance.now();
 
-        let turn = numbers.length;
-        let lastSpoken = numbers[turn - 1];
-        let t1, t2;
+        // Keep an array with two slots for each number that has been spoken. 
+        // The two slots holds the last two turns that the number was spoken.
+        // If both slots are 0, the number has not been spoken. If only the
+        // second slot is 0, the number has been spoken once. 
+
+        //const lastSpokenTurns = new Array(turns * 2).fill(0);
+        const lastSpokenTurns = new Uint32Array(turns * 2);
+        //const lastSpokenTurns = new Int32Array(turns * 2);
+        //const lastSpokenTurns = new Float64Array(turns * 2);
+
+        startingNumbers.forEach((number, index) => lastSpokenTurns[number * 2] = index + 1);
+
+        let turn = startingNumbers.length;
+        let lastSpoken = startingNumbers[turn - 1];
+        let t1 = lastSpokenTurns[lastSpoken * 2];
+        let t2 = 0;
+
         while (turn++ < turns) {
-            t1 = lastSpokenTurnPairs[lastSpoken * 2];
-            t2 = lastSpokenTurnPairs[lastSpoken * 2 + 1];
-            if (t2 === 0) {
-                lastSpoken = 0;
-            } else {
-                lastSpoken = Math.abs(t1 - t2);
-            }
-            t1 = lastSpokenTurnPairs[lastSpoken * 2];
-            t2 = lastSpokenTurnPairs[lastSpoken * 2 + 1];
+            lastSpoken = t2 === 0 ? 0 : Math.abs(t1 - t2);
+            const t1Slot = lastSpoken * 2;
+            const t2Slot = t1Slot + 1;
+            t1 = lastSpokenTurns[t1Slot];
+            t2 = lastSpokenTurns[t2Slot];
             if (t1 > t2) {
-                lastSpokenTurnPairs[lastSpoken * 2 + 1] = turn;
+                lastSpokenTurns[t2Slot] = turn;
+                t2 = turn;
             } else {
-                lastSpokenTurnPairs[lastSpoken * 2] = turn;
+                lastSpokenTurns[t1Slot] = turn;
+                t1 = turn;
             }
         }
+
+        // console.log(`Time for ${turns} turns with starting numbers ${input} is`, performance.now() - start, "ms, with", lastSpokenTurns.constructor.name);
+
         return lastSpoken;
     }
 

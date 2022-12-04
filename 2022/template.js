4ec0c2a432f6fff801ab222eb0db833fa1737113
Svenn-Ivar Svendsen <svennivar.svendsen@microchip.com>
2022-12-04 21:43:27 +0100

    Day 1-4 2022

---
+++ 2022/template.js
@@ -0,0 +1,18 @@
+const solve = (input) => {
+
+
+
+    const result1 = 0;
+    const result2 = 0;
+
+    return [result1, result2];
+}
+
+const example = 
+``;
+
+const challenge = 
+``;
+
+console.log("Example", solve(example)); 
+console.log("Challenge", solve(challenge)); 

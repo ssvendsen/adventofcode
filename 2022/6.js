39471ccaf16f25f3d1e467d77193fcf8c591523f
Svenn-Ivar Svendsen <svennivar.svendsen@microchip.com>
2022-12-06 23:21:35 +0100

    Make day a bit more readable

--- 2022/6.js
+++ 2022/6.js
@@ -5,26 +5,26 @@ const solve = (input) => {
     // scan a sequence of characters and return an array of the same length,
     // where each entry is the distance from the character in that position in 
     // the input, to the next same character in the input
-    const getDeltas = (sequence) => {
+    const getDistancesToSameChar = (sequence) => {
         const charPos = {};
-        const deltas = Array(sequence.length).fill(Number.MAX_SAFE_INTEGER);
+        const distances = Array(sequence.length).fill(Number.MAX_SAFE_INTEGER);
         sequence.forEach((char, pos) => {
             const lastCharPos = charPos[char];
             if (lastCharPos !== undefined) {
-                deltas[lastCharPos] = pos - lastCharPos;
+                distances[lastCharPos] = pos - lastCharPos;
             }
             charPos[char] = pos;
         });
-        return deltas;
+        return distances;
     }
 
     // scan for a sub sequence of unique characters of a given length
     const findUniqueSubSequence = (sequence, length) => {
-        const deltas = getDeltas(sequence);
-        let count = 0;
-        return deltas.findIndex(delta => {
-            count = (count + delta) >= length ? count + 1 : 0;
-            return count > length;
+        const distances = getDistancesToSameChar(sequence);
+        let uniqueSeqLength = 0;
+        return distances.findIndex(distance => {
+            uniqueSeqLength = (uniqueSeqLength + distance) >= length ? uniqueSeqLength + 1 : 0;
+            return uniqueSeqLength > length;
         })
     }
 

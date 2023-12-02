4ca4309cbc94151e46fe0c3c8592a365eca2f7e5
Svenn-Ivar Svendsen <svennivar.svendsen@microchip.com>
2023-12-02 09:47:47 +0100

    Day 2 2023

--- 2023/template.js
+++ 2023/template.js
@@ -1,29 +1,15 @@
 const solve = (input) => {
 
-    const lines = input.split("\n");
 
-    const getFirstAndLastDigit = (line) => {
-        const digits = line.split("");
-        const first = digits.find(d => !isNaN(d))
-        const last = digits.findLast(d => !isNaN(d))
-        return parseInt(`${first}${last}`);
-    }
 
-    const calibrations = lines.map(getFirstAndLastDigit);
-
-    const sum = calibrations.reduce((sum, item) => sum + item, 0);
-
-    const result1 = sum;
+    const result1 = 0;
     const result2 = 0;
 
     return [result1, result2];
 }
 
 const example = 
-`1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet`;
+``;
 
 const challenge = 
 ``;
